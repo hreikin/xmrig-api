c1a18c3a635ff2c7a467dc45389ff03d55dbb668
hreikin <hreikin@gmail.com>
2024-12-12 09:32:04 +0000

    Update imports.

--- xmrig/__init__.py
+++ xmrig/__init__.py
@@ -4,8 +4,8 @@ XMRig module initializer.
 This module provides objects to interact with the XMRig miner API.
 """
 
-from api import XMRigAPI
-from manager import XMRigManager
+from .api import XMRigAPI, XMRigAuthorizationError
+from .manager import XMRigManager
 
 __name__ = "xmrig"
 __version__ = "0.0.3"
