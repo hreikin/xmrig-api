f8609f87bc306c8dc367514b3f57515fa9beae1a
hreikin <hreikin@gmail.com>
2024-12-22 17:06:23 +0000

    Add database interaction functions for JSON data handling

---
+++ xmrig/db.py
@@ -0,0 +1,42 @@
+import json
+import pandas as pd
+from sqlalchemy import create_engine, text
+from datetime import datetime
+from xmrig.logger import log
+
+_engines = {}
+
+def init_db(db_url):
+    if db_url not in _engines:
+        _engines[db_url] = create_engine(db_url)
+    return _engines[db_url]
+
+def insert_data_to_db(json_data, table_name, engine):
+    # Normalize nested JSON
+    df = pd.json_normalize(json_data)
+
+    # Convert lists to JSON strings
+    for column in df.columns:
+        if df[column].apply(lambda x: isinstance(x, list)).any():
+            df[column] = df[column].apply(json.dumps)
+
+    # Add a timestamp column
+    df.insert(0, 'timestamp', datetime.now())
+
+    # Insert data into the database
+    df.to_sql(table_name, engine, if_exists='append', index=False)
+
+    log.debug("Data inserted successfully")
+
+def delete_miner_from_db(miner_name, engine):
+    # Use quotes to avoid SQL syntax errors
+    backends_table = f"'{miner_name}-backends'"
+    config_table = f"'{miner_name}-config'"
+    summary_table = f"'{miner_name}-summary'"
+    with engine.connect() as connection:
+        # Wrap the raw SQL strings in SQLAlchemy's `text` function so it isn't a raw string
+        connection.execute(text(f"DROP TABLE IF EXISTS {backends_table}"))
+        connection.execute(text(f"DROP TABLE IF EXISTS {config_table}"))
+        connection.execute(text(f"DROP TABLE IF EXISTS {summary_table}"))
+
+    log.debug(f"All tables for '{miner_name}' have been deleted from the database")
