21234b137edb8d158c5dd22e76244e3f0156510f
hreikin <hreikin@gmail.com>
2024-12-23 18:26:50 +0000

    Bump version to 0.0.8

--- xmrig/__init__.py
+++ xmrig/__init__.py
@@ -220,7 +220,7 @@ from .manager import XMRigManager
 from .helpers import XMRigAPIError, XMRigAuthorizationError, XMRigConnectionError
 
 __name__ = "xmrig"
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __author__ = "hreikin"
 __email__ = "hreikin@gmail.com"
 __license__ = "MIT"
