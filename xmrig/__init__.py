d069d9aaa98352fdf95d1f52bb0ffbd31b52b469
hreikin <hreikin@gmail.com>
2025-02-07 15:07:39 +0000

    Bump version to 0.2.3

--- xmrig/__init__.py
+++ xmrig/__init__.py
@@ -357,7 +357,7 @@ from .db import XMRigDatabase
 from .exceptions import XMRigAPIError, XMRigAuthorizationError, XMRigConnectionError, XMRigDatabaseError, XMRigManagerError
 
 __name__ = "xmrig"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "hreikin"
 __email__ = "hreikin@gmail.com"
 __license__ = "MIT"
